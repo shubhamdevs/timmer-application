25d05cbeb1b030d5c85bdb388dc187428b1b542f
SHUBHAM BISWAS <suvm.dev@gmail.com>
2024-03-04 20:36:41 +0530

    create some optional callbacks

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 class Timer {
-  constructor(durationInput, startButton, pauseButton) {
+  constructor(durationInput, startButton, pauseButton, callbacks) {
     this.durationInput = durationInput;
     this.startButton = startButton;
     this.pauseButton = pauseButton;
+    if (callbacks) {
+      this.onStart = callbacks.onStart;
+      this.onTick = callbacks.onTick;
+      this.onComplete = callbacks.onComplete;
+    }
 
     this.timeLeft = 30;
 
@@ -11,24 +16,55 @@ class Timer {
   }
 
   start = () => {
+    if (this.onStart) {
+      this.onStart();
+    }
     this.tick();
     this.interval = setInterval(this.tick, 1000);
-    console.log(timer);
+    
   };
 
   pause = () => {
     clearInterval(this.interval);
   };
 
-  tick = () => {
-    this.timeLeft -= 1;
-    this.durationInput.value = this.timeLeft;
-    console.log("tick");
+  tick = () => {  
+
+    if (this.timeRemaining <= 0) {
+      this.pause()
+      if (this.onComplete) {
+        this.onComplete();
+      }
+    } else {
+      this.timeRemaining = this.timeRemaining - 1;
+      if (this.onTick) {
+        this.onTick();
+      }
+    }
+    
   };
+
+  get timeRemaining() {
+    return parseFloat(this.durationInput.value);
+  }
+
+  set timeRemaining(time) {
+    this.durationInput.value = time;
+  }
 }
 
 const durationInput = document.querySelector("#duration");
 const startButton = document.querySelector("#start");
 const pauseButton = document.querySelector("#pause");
 
-const timer = new Timer(durationInput, startButton, pauseButton);
+const timer = new Timer(durationInput, startButton, pauseButton, {
+  onStart() {
+    console.log("Timer started")
+  },
+  onTick() {
+    console.log("Timer just ticked down")
+  },
+  onComplete() {
+    console.log("Timer is completed")
+  }
+});
