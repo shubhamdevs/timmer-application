fe6af48ea51717edfe635f9e420c96e158394bb7
SHUBHAM BISWAS <suvm.dev@gmail.com>
2024-03-04 20:08:37 +0530

    connect duration with buttons

--- index.js
+++ index.js
@@ -1,22 +1,34 @@
 class Timer {
-    constructor(durationInput, startButton, pauseButton) {
-        this.durationInput = durationInput;
-        this.startButton = startButton;
-        this.pauseButton = pauseButton;
+  constructor(durationInput, startButton, pauseButton) {
+    this.durationInput = durationInput;
+    this.startButton = startButton;
+    this.pauseButton = pauseButton;
 
-        this.startButton.addEventListener('click', this.start);
-        
-    }
+    this.timeLeft = 30;
 
-    start() {
-        console.log('Time to start the timer!')
-    }
+    this.startButton.addEventListener("click", this.start);
+    this.pauseButton.addEventListener("click", this.pause);
+  }
+
+  start = () => {
+    this.tick();
+    this.interval = setInterval(this.tick, 1000);
+    console.log(timer);
+  };
+
+  pause = () => {
+    clearInterval(this.interval);
+  };
+
+  tick = () => {
+    this.timeLeft -= 1;
+    this.durationInput.value = this.timeLeft;
+    console.log("tick");
+  };
 }
-
 
 const durationInput = document.querySelector("#duration");
 const startButton = document.querySelector("#start");
 const pauseButton = document.querySelector("#pause");
 
-
-const timer = new Timer(durationInput, startButton, pauseButton)
+const timer = new Timer(durationInput, startButton, pauseButton);
